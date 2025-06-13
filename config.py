cbf9bef89682281546051502bf116d896499b46b
HAFIZ RAFIE ADITYA <73168585+hfzrfa@users.noreply.github.com>
2025-06-13 19:09:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Copyright © 2020 by Spectrico
-# Licensed under the MIT License
-
 model_file = "model-weights-spectrico-car-colors-recognition-mobilenet_v3-224x224-180420.pb"  # path to the car color classifier
 label_file = "labels.txt"   # path to the text file, containing list with the supported makes and models
 input_layer = "input_1"
